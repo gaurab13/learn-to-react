c96681bfd27911d063a926ea8b3c3f7d7b73b975
Gaurav KC <13gaurab@gmail.com>
2019-01-20 22:17:35 +0545

    Add list of attendees

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import Attendees from './Attendees';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      attendees: [
+        {
+          id: 'person1',
+          name: 'Rick',
+          address: 'Washington'
+        },
+        {
+          id: 'person2',
+          name: 'Bender',
+          address: 'New York'
+        },
+        {
+          id: 'person3',
+          name: 'Fry',
+          address: 'New New York'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>React Workshop</h1>
-        <Attendees name="Rick" address="Washington" />
+        <Attendees attendees={this.state.attendees} />
       </div>
     );
   }
--- myapp/src/Attendees.jsx
+++ myapp/src/Attendees.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 class Attendees extends Component {
   render() {
+    const { attendees } = this.props;
+    const list = attendees.map( attendee => {
+      return (
+        <div className="attendee" key={attendee.id}>
+          <p>{`Name: ${attendee.name}`}</p>
+          <p>{`Address: ${attendee.address}`}</p>
+        </div>
+      )
+    })
     return (
-      <div className="attendees">
-        <p>{`Name: ${this.props.name}`}</p>
-        <p>{`Address: ${this.props.address}`}</p>
+      <div>
+        {list}
       </div>
     )
   }
